97c642c31ea7313f907d44a644708847cd9a3e24
Bernardo Lobato <bernardo@pop-os.localdomain>
2021-03-17 20:01:49 -0300

    first commit, spring initilzr

---
+++ src/main/java/br/bernardolobato/wsbeerservice/WsBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package br.bernardolobato.wsbeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsBeerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsBeerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/bernardolobato/wsbeerservice/WsBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.bernardolobato.wsbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsBeerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
